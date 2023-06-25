c08f896792cc014632c4d05f641e72b18e3f7554
divyakrishnan15 <divyabalajee@gmail.com>
2023-06-25 12:36:07 -0400

    new

--- ReadMe_Generator_nodejs/utils/generateMarkdown.js
+++ ReadMe_Generator_nodejs/utils/generateMarkdown.js
@@ -1,57 +1,81 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicense(license) {
-  let yourLicense = "";
-  console.log(license);
+  var yourLicense;
+  // console.log(license);
+
   for (let l in license) {
     console.log(license[l]);
     switch (license[l]) {
       case "MIT": {
-        yourLicense += `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`;
-        console.log("GOOD");
+        yourLicense = `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`;
         break;
       }
-      case "Javascript": {
-        yourLicense += `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`;
+      case "Apache": {
+        yourLicense = `![License: Apache](https://img.shields.io/badge/License-Apache%202.0-blue.svg)`;
         break;
       }
-      case "Eclipse": {
-        yourLicense += `[![License: Eclipse](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)`;
+      case "Mozilla": {
+        yourLicense = `![License: Mozilla](https://img.shields.io/badge/License-MPL%202.0-orange.svg)`;
         break;
       }
     }
   }
-  return `${yourLicense}`;
+  console.log(yourLicense)
+  return yourLicense;
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseLink(license) {
+  // console.log(' renderLicenseLink = ',license)
+  // console.log(' renderLicenseLink = ',String(license))
+  var licenseName;
+  var licenseurl;
+
+  var licenseNew = String(license)
+
+  switch (licenseNew) {
+    case "MIT":
+      licenseName = "MIT License";
+      licenseurl = "https://choosealicense.com/licenses/mit/";
+      break;
+    case "Mozilla":
+      licenseName = "Mozilla Public License 2.0";
+      licenseurl = "https://choosealicense.com/licenses/mpl-2.0/";
+      break;
+    case "Apache":
+      licenseName = "Apache License 2.0";
+      licenseurl = "https://choosealicense.com/licenses/apache-2.0/";
+      break;
+    default:
+      licenseName = "No License selected";
+      licenseurl = "";
+      break;
+}
+// console.log('licenseName ', licenseName)
+// console.log('licenseurl = ',licenseurl)
+// console.log(`END FUNC = [${licenseName}](${licenseurl})`)
+return `[${licenseName}](${licenseurl})`;
+}
+
+
+// function renderLicenseSection(license) {
+//   let licenseSection = "";
+//   if (license != "NONE") {
+//     licenseSection += "## License\n";
+//     licenseSection += renderLicenseLink(license)
+    
+//   }
+//   return licenseSection;
+// }
+
 
 // TODO: Create a function to generate markdown for README
 
-function renderProgress(license) {
-  let progress = "";
-  console.log(license);
-  // for (let l in license) {
-  console.log(license[0]);
-  switch (license[0]) {
-    case "Done": {
-      progress = `DONE`;
-      break;
-    }
-    case "In Progress": {
-      progress = `In Progress`;
-      break;
-    }
-  }
-  
-  return `${progress}`;
-}
 
 
 
@@ -62,27 +86,23 @@ function renderLink() {
       `1.  [Documentation](#documentation) \n
         1.  [Title](#Title) \n
         2.  [Description](#Description) \n
-        3.  [UserStory](#UserStory) \n
-        4.  [AcceptanceCriteria](#AcceptanceCriteria) \n
-        5.  [Questions](#Questions) \n
-        6.  [Installation](#Installation) \n
-        7.  [Usage](#Usage) \n
-        8.  [Video](#Video) \n
-        9.  [License](#License) \n
-        10. [Screenshot](#Screenshot) \n
-        11.  [Tests](#Tests) \n`
+        3.  [Questions](#Questions) \n
+        4.  [Installation](#Installation) \n
+        5.  [Usage](#Usage) \n
+        6.  [Video](#Video) \n
+        7.  [License](#License) \n
+        8. [Tests](#Tests) \n
+        9. [Screenshot](#screenshot) \n`
   return `${link}`
 }
-// var readme = `${title} ${nodebadge} ${jsbadge} ${inquirerbadge} ${toc} ${desc} ${userstory} ${acceptcriteria} ${questions} ${answers} ${installation} ${usage} ${watchmevideo} ${license} ${screenshot} ${tests} ${copyright}`
-   // Title Description UserStory AcceptanceCriteria githubusername emailaddress Gitrepo Installation Usage Video Screenshot
 
 
 module.exports = {
   license: function (license) {
     return renderLicense(license);
   },
-  progress: function (progress) {
-    return renderProgress(progress)
+  licenseLink: function (license) {
+    return renderLicenseLink(license) //s:["MIT","Apache","Mozilla"
   },
   toc: function (toc) {
     return renderLink(toc)
